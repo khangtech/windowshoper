79f0925e23551195611dedae0f5dfbb5324d1564
= <=>
2019-04-19 20:04:49 +0700

    Newproject

---
+++ WindowShoper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WindowShoper
+//
+//  Created by Pham Nguyen Hai An on 4/19/19.
+//  Copyright © 2019 KhangTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
